fa52261e2c69a6c89bd516f250c7df5f535b0515
pedroalegre <alegre.p@gmail.com>
2017-03-17 09:54:30 +0100

    Responsive done.

--- js/main.js
+++ js/main.js
@@ -11,14 +11,15 @@ $(document).ready(function() {
 // place all the books
 function showBooks(books) {
 	$.each(books, function(i) {
-		$("#booksList").append("<div class='book well'>");
-		$(".book").last().append("<img src='" + books[i].portada + "' alt='bookCover' width='248.4px'>");
+		$("#booksList").append("<div class='book well col-xs-12'>");
+		$(".book").last().append("<div class='bookDescription overlay'>" + books[i].descripcion + "</div>");
+		$(".book").last().append("<img src='" + books[i].portada + "' alt='bookCover' width='248.4px' height='248.4px'>");
 		$(".book").last().append("<div class='bookTitle'>" + books[i].titulo + "</div>");
 		$(".book").last().append("<div>Idioma: " + "<span class='text-uppercase'>" + books[i].idioma + "</span></div>");
-		$(".book").last().append("<div class='bookDescription'>" + books[i].descripcion + "</div>");
 	})
 }
 
+// load the booklist
 function updateBookList(val) {
 	$.getJSON("https://micuento.com/test/get_cuentos/" + val, function(data) {
 		$("#booksList").empty();
